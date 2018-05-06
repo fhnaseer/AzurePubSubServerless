a61c3108c9dd4f3cea540083bbf235046436a645
faisal <fhnaseer@live.com>
2018-05-06 17:48:24 +0200

    publishing new functions to azure portal,

--- .funcpack/index.gen.js
+++ .funcpack/index.gen.js
@@ -1,7 +1,9 @@
 module.exports = {
     "PublishContent": require("../PublishContent/index.js"),
+    "PublishFunction": require("../PublishFunction/index.js"),
     "PublishTopic": require("../PublishTopic/index.js"),
     "Subscribe": require("../Subscribe/index.js"),
     "SubscribeContent": require("../SubscribeContent/index.js"),
+    "SubscribeFunction": require("../SubscribeFunction/index.js"),
     "SubscribeTopic": require("../SubscribeTopic/index.js")
 }
