2e746b4f98c421173444d443a1cc9374b0898351
faisal <fhnaseer@live.com>
2018-05-08 20:47:40 +0200

    packing functions for publihing,

---
+++ .funcpack/index.gen.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "PublishContent": require("../PublishContent/index.js"),
+    "PublishFunction": require("../PublishFunction/index.js"),
+    "PublishTopic": require("../PublishTopic/index.js"),
+    "Subscribe": require("../Subscribe/index.js"),
+    "SubscribeContent": require("../SubscribeContent/index.js"),
+    "SubscribeFunction": require("../SubscribeFunction/index.js"),
+    "SubscribeTopic": require("../SubscribeTopic/index.js"),
+    "UnregisterSubscriber": require("../UnregisterSubscriber/index.js")
+}
