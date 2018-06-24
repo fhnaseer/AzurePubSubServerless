a71ed04a2a0b4c33d05951a2a3d58678cd95b643
fhnaseer <fhnaseer@live.com>
2018-06-24 23:30:14 +0200

    subscribe function now requires function type as input, this can be string/url,

--- SubscribeFunction/index.js
+++ SubscribeFunction/index.js
@@ -1,30 +1,33 @@
 const common = require('../Shared/common');
 
 let subscriberId = '';
-let subscriptionType = '';
+let functionType = '';
+let subscriptionTopic = '';
 let matchingInputs = '';
 let matchingFunction = '';
 
-module.exports = function(context, req) {
+module.exports = function (context, req) {
   if (req.body) {
     subscriberId = req.body.subscriberId;
-    subscriptionType = req.body.subscriptionType;
+    functionType = req.body.functionType;
+    subscriptionTopic = req.body.subscriptionTopic;
     matchingInputs = req.body.matchingInputs;
     matchingFunction = req.body.matchingFunction;
     addTableData();
     common.sendQueueConnectionResponse(context, subscriberId);
   } else {
-    common.sendErrorResponse(context, 'Please pass subscriberId, subscriptionType, matchingInputs and matchingFunction in the request body');
+    common.sendErrorResponse(context, 'Please pass subscriberId, funtionType, subscriptionTopic, matchingInputs and matchingFunction in the request body');
   }
 };
 
 function addTableData() {
   var tableService = common.getTableService();
   var task = {
-    PartitionKey: { _: subscriptionType },
+    PartitionKey: { _: subscriptionTopic },
     RowKey: { _: subscriberId },
-    MatchingInputs: { _: matchingInputs },
-    MatchingFunction: { _: matchingFunction }
+    FunctionType: { _: functionType },
+    MatchingFunction: { _: matchingFunction },
+    MatchingInputs: { _: matchingInputs }
   };
-  tableService.insertEntity(common.functionTableName, task, function(error, result, response) {});
+  tableService.insertEntity(common.functionTableName, task, function (error, result, response) { });
 }
