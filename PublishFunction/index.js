793dc884ed56358c3e0032ae9c5856951f1681df
fhnaseer <fhnaseer@live.com>
2018-06-30 22:12:49 +0200

    publish function now checks for function type and acts accordingly,

--- PublishFunction/index.js
+++ PublishFunction/index.js
@@ -1,10 +1,11 @@
 const common = require('../Shared/common');
 const azureStorage = require('azure-storage');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 let subscriptionType = '';
 let message = '';
 
-module.exports = function(context, req) {
+module.exports = function (context, req) {
   if (req.body) {
     message = req.body.message;
     subscriptionType = req.body.subscriptionType;
@@ -18,8 +19,8 @@ module.exports = function(context, req) {
 
 function queryTableData(context) {
   var tableService = common.getTableService();
-  var query = new azureStorage.TableQuery().select(['PartitionKey', 'RowKey', 'MatchingInputs', 'MatchingFunction']).where('PartitionKey eq ?', subscriptionType);
-  tableService.queryEntities(common.functionTableName, query, null, function(error, result, response) {
+  var query = new azureStorage.TableQuery().select(['PartitionKey', 'RowKey', 'FunctionType', 'MatchingInputs', 'MatchingFunction']).where('PartitionKey eq ?', subscriptionType);
+  tableService.queryEntities(common.functionTableName, query, null, function (error, result, response) {
     if (error) {
       common.sendErrorResponse(context, error);
     } else {
@@ -31,15 +32,30 @@ function queryTableData(context) {
 function publishMessages(topics) {
   var serviceBusService = common.getServiceBusService();
   topics.map(topic => {
-    let matchingFunction = new Function(topic.MatchingInputs, topic.MatchingFunction);
-    let value = matchingFunction(message);
-    if (value) {
-      let completeMessage = {
-        topic: topic.PartitionKey,
-        message: message,
-        value: value
-      };
-      serviceBusService.sendQueueMessage(topic.RowKey, JSON.stringify(completeMessage), function(error) {});
+    if (topic.FunctionType === 'string') {
+      let matchingFunction = new Function(topic.MatchingInputs, topic.MatchingFunction);
+      let value = matchingFunction(message);
+      if (value) {
+        let completeMessage = {
+          topic: topic.PartitionKey,
+          message: message,
+          value: value
+        };
+        serviceBusService.sendQueueMessage(topic.RowKey, JSON.stringify(completeMessage), function (error) { });
+      }
+    } else if (topic.FunctionType === 'url') {
+      const Http = new XMLHttpRequest();
+      const url = topic.MatchingFunction;
+      Http.open("GET", url);
+      Http.send();
+      Http.onreadystatechange = (e) => {
+        let completeMessage = {
+          topic: topic.PartitionKey,
+          message: message,
+          value: Http.responseText
+        };
+        serviceBusService.sendQueueMessage(topic.RowKey, JSON.stringify(completeMessage), function (error) { });
+      }
     }
   });
 }
